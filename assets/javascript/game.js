58464a6d34d0e172ad00d32c63a56ca5b6e6c575
Hannah Jacoby <hjacoby@grubhub.com>
2019-07-07 16:04:53 -0600

    js work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,60 +2,56 @@
   //  variables
       var wins = 0
       var losses = 0;
-
+      var words = ["red", "green", "blue"];
       var underScore = [];
-
-      var chosenWords = ["red", "green", "blue"];
-      var correctWord =[];
-      var incorrectWord =[];
+      var randNum = Math.floor(Math.random()* words.length)
+      var chosenWord = words[randNum];
+      console.log(chosenWord)
       var userGuess = [];
+      var correctWord = [];
+      var incorrectWord = [];
 
     // dom manipulation
-
     var actualUnderScore = document.getElementsByClassName ("underScore");
     var actualRightGuess = document.getElementsByClassName ("rightGuess");
+    var actualWrongGuess = document.getElementsByClassName ("wrongGuess");
   
-    //   // replacing underscores with letters 
-    //   var randNum = Math.floor(Math.random()* chosenWords.length);
-    //   var chosenWords = chosenWords[randNum];
-    //   console.log(chosenWords)
-
-    //    for (i = 0; i < chosenWords.length; i++) {
-    //     underScore = underScore.replace("_", chosenWords.charAt(i));
-    // }
-    //   return underScore
+    console.log(chosenWord)
+      // replacing underscores with letters 
+      function generateUnderScore() {
+      for (var i = 0; i < word.length; i++) {
+        underScore.push('_');
+      }
+        return underScore; 
+    }
     
-    //   var replaceletter = 0
-    //             for (i = 0; i < chosenWords.length; i++)   {
-    //                 replaceletter = chosenWords.indexOf(letter, replaceletter);
-    //                 underScore = underScore.replace(underscore.charAt(replaceletter), word.charAt(replaceletter));
-    //             }
-
-                
-              
+    console.log(generateUnderScore)
+  
+      
               
       // user presses key, key response logged
       document.addEventListener('keyup', function(event) {
-        var userGuess = String.fromCharCode(event.keyCode);
+        var userGuess =(event.key);
         console.log(userGuess)
       });
 
       // user guess is correct
-      if(userGuess.indexOf(chosenWords)> -1) {
+      if(words.indexOf(userGuess)> -1) {
       // adding to 'correctword/incorrectword' array
       correctWord.push(userGuess);
-      underScore[userGuess.indexof(chosenWords)] = chosenWords;
-      actualUnderScore[0].innerHTML = underScore.join("");  
-      actualRightGuess[0].innerHTML = rightGuess.join("");
+
+      underScore[chosenWords.indexof(userGuess)] = userGuess;
+        console.log(underscore)
+        actualUnderScore[0].innerHTML = underScore.join(""); 
+      actualRightGuess[0].innerHTML = rightGuess;
+      
       // userGuess letters join together to make word if correct
-      if (underScore.join("") === chosenWords){
-        alert("win");
-      }
-      console.log(underScore)  
+      } if (underScore.join("") === chosenWord){
+       
       } else { (incorrectWord.push(userGuess))
-        
+        actualWrongGuess[0].innerHTML = incorrectWord.join("");
+
       }
-      actualUnderScore[0].innerHTML = generateUnderScore().join(" ");
       
   // run underscore function 
       
